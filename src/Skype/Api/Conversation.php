2d72cdb2c5adbe6b3fbf40dac8e1c85a09f9426b
Radu Topala <radu.topala@trisoft.ro>
2018-02-14 14:39:48 +0000

    added suggested actions to activity

--- src/Skype/Api/Conversation.php
+++ src/Skype/Api/Conversation.php
@@ -8,16 +8,23 @@ class Conversation extends BaseApi implements ApiInterface
      * Sends an activity message
      *
      * @param string $target In format of 8:<username> or 19:<group>
-     * @param string $content The message
+     * @param string $text The message
+     * @param array $suggestedActions
      * @return \Psr\Http\Message\ResponseInterface
      */
-    public function activity($target, $content)
+    public function activity($target, $text, $suggestedActions = [])
     {
+        $json = [
+            'type' => 'message/text',
+            'text' => $text,
+        ];
+
+        if (!empty($suggestedActions)) {
+            $json['suggestedActions']['actions'] = $suggestedActions;
+        }
+
         return $this->request('POST', '/v3/conversations/' . $target . '/activities', [
-            'json' => [
-                "type" => "message/text",
-                "text" => $content
-            ]
+            'json' => $json
         ]);
     }
 }
